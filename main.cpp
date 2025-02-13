7ca1d5352de5adfd3cc9aa045e8209decc24efa0
Donghan Kim <donghank@made-f0Cr10s6.clusters.42paris.fr>
2025-02-13 15:37:31 +0100

    add the key

--- main.cpp
+++ main.cpp
@@ -3,11 +3,21 @@
 /*
 	pesudo maybe
 */
+
+void ClassName::parseConfigure(std::string pathToConfFile)
+{
+	std::ifstream file(pathToConfFile);
+	std::string infoLine;
+	std::stringstream ss(infoline);
+	if (fileNotOpen)
+		throw FileNotOpenException();
+	while (std::getline(file, ss))
+}
 void ClassName::startServer(???) {
-	int serverSocket = createSocket();
+	int serverSocket = createSocket(); // using socket(AF_INET, SOCK_STREAM, 0)
 	if (serverSocket == -1) throw FailureSocketCreate();
 	while (true) {
-		int clientSocket = acceptConnect();
+		int clientSocket = acceptConnect(); // listen(serversocket, 4) accept(serverSocket, NULL, NULL)
 		if (clinetSocket == -1) throw FailureSocketAccess();
 		parseRequest();
 
