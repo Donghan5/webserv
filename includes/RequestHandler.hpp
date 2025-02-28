4dd71d1a87a4ba89039d4d333a10cacd5d10e0f8
Donghan Kim <donghank@paul-f4Ar6s6.clusters.42paris.fr>
2025-02-28 16:49:14 +0100

    sep and integrate serv2.cpp to this httpserver delete webserv

--- includes/RequestHandler.hpp
+++ includes/RequestHandler.hpp
@@ -14,6 +14,7 @@ class RequestHandler {
 		static bool file_exists(const std::string &path);
 		static bool ends_with(const std::string &str, const std::string &suffix);
 		static std::string process_request(const std::string &request);
+		//sdsfsdf
 };
 
 #endif
